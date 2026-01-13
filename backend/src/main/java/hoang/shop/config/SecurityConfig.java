5392214f41764dbdd3f55a3bdd4ce7ae70fcc32e
Hoang <2240712@ecc.ac.jp>
2026-01-13 16:07:22 +0900

    Fix register page layout

--- backend/src/main/java/hoang/shop/config/SecurityConfig.java
+++ backend/src/main/java/hoang/shop/config/SecurityConfig.java
@@ -81,7 +81,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowedOrigins(List.of("https://hoangdev.com",
-                "https://www.hoangdev.com", "http://127.0.0.1:5500", "http://localhost:5500"));
+                "https://www.hoangdev.com","https://api.hoangdev.com",
+                "https://www.api.hoangdev.com", "http://127.0.0.1:5500", "http://localhost:5500"));
 
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type", "Accept"));
